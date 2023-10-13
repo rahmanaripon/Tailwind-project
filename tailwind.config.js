ab62d01567172d4b8bcfa4548e2b1b00bd037dc8
rahmanaripon <rjripon268@gmail.com>
2023-10-13 20:52:42 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // prettier.config.js
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+  ],
+}
+
